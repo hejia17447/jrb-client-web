ae045af384feb82982cc57fbf941db9996ac040d
HJ <1744709356@qq.com>
2023-07-03 22:38:33 +0800

    整合自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 const pathSrc = path.resolve(__dirname, "src")
 
@@ -17,6 +19,24 @@ export default defineConfig(({ command, mode }) => {
       }
     },
     server: {},
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ["vue"],
+        eslintrc: {
+          // 是否自动生成 eslint 规则，建议生成之后设置 false 
+          enabled: true, 
+          // 指定自动导入函数 eslint 规则的文件
+          filepath: "./.eslintrc-auto-import.json", 
+        },
+        // 指定自动导入函数TS类型声明文件路径
+        dts: path.resolve(pathSrc, "types", "auto-imports.d.ts"), 
+      }),
+      Components({
+        // 指定自动导入组件TS类型声明文件路径
+        dts: path.resolve(pathSrc, "types", "components.d.ts"), 
+      }),
+    ],
   }
 })
