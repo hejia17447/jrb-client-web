f8182bd4327c6b459f1a3fc47befe385d929268e
HJ <1744709356@qq.com>
2023-07-04 09:48:34 +0800

    整合 Element Plus

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 const pathSrc = path.resolve(__dirname, "src")
 
@@ -30,13 +33,33 @@ export default defineConfig(({ command, mode }) => {
           // 指定自动导入函数 eslint 规则的文件
           filepath: "./.eslintrc-auto-import.json", 
         },
+        resolvers: [
+          // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({}),
+        ],
+        vueTemplate: true,
         // 指定自动导入函数TS类型声明文件路径
         dts: path.resolve(pathSrc, "types", "auto-imports.d.ts"), 
       }),
       Components({
+        resolvers: [
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+            enabledCollections: ["ep"] 
+          }),
+        ],
         // 指定自动导入组件TS类型声明文件路径
         dts: path.resolve(pathSrc, "types", "components.d.ts"), 
       }),
+      Icons({
+        // 自动安装图标库
+        autoInstall: true
+      })
     ],
   }
 })
